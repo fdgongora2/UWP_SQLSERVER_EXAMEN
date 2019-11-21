c0e8629e50b52cbc066d02381d1fd670ad4745d9
David Góngora <fdgongora@iesmurgi.org>
2019-11-21 06:38:31 +0100

    Pruebas con imágenes

---
+++ APP_UWP_Imagenes/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace APP_UWP_Imagenes
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private  async void CargarImagen_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            var bitmap = new BitmapImage();
+            using (var stream = await file.OpenReadAsync())
+            {
+                await bitmap.SetSourceAsync(stream);
+            }
+            Imagen_Mostrada.Source = bitmap;
+
+        }
+    }
+}
--- UWP_AccesoDatos_SQLServer_Northwind_Empleados/EditarEmpleado.xaml.cs
+++ UWP_AccesoDatos_SQLServer_Northwind_Empleados/EditarEmpleado.xaml.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using UWP_AccesoDatos_SQLServer_Northwind_Empleados.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
@@ -72,6 +75,46 @@ namespace UWP_AccesoDatos_SQLServer_Northwind_Empleados
         {
             this.Frame.Navigate(typeof(MainPage));
 
+        }
+
+        private async void Img_Empleado_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                var bitmap = new BitmapImage();
+                using (var stream = await file.OpenReadAsync())
+                {
+                    await bitmap.SetSourceAsync(stream);
+
+                }
+
+
+                Img_Empleado.Source = bitmap;
+
+                Empleado_Actual.Photo = await ImageToBytes (bitmap);
+
+            }
+        }
+
+        public async static Task<byte[]> ImageToBytes(BitmapImage image)
+        {
+            RandomAccessStreamReference streamRef = RandomAccessStreamReference.CreateFromFile(image);
+            IRandomAccessStreamWithContentType streamWithContent = await streamRef.OpenReadAsync();
+            byte[] buffer = new byte[streamWithContent.Size];
+            await streamWithContent.ReadAsync(buffer.AsBuffer(), (uint)streamWithContent.Size, InputStreamOptions.None);
+            return buffer;
         }
     }
 }
